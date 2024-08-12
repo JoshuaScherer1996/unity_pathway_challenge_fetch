47087dd4847245224b69dc68823b5624e84a15b6
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-12 15:33:37 +0200

    randomized the spawn time and restructered the SpawnManagerX script

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,29 +9,46 @@ public class SpawnManagerX : MonoBehaviour
     private const float SpawnPosY = 30;
 
     private const float StartDelay = 1.0f;
-    private const float SpawnInterval = 4.0f;
+    private float _randomSpawnTime;
 
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating(nameof(SpawnRandomBall), StartDelay, SpawnInterval);
+        // Invoking the method to randomize the spawn times.
+        RandomizeInterval();
+        // Using Invoke instead of InvokeRepeating to be able to use dynamic time intervals.
+        Invoke(nameof(SpawnRandomBall), StartDelay);
     }
 
-    // Spawn random ball at random x position at top of play area
+    // Spawn random ball at random x position at top of play area.
     private void SpawnRandomBall()
     {
-        // Generate random ball index and random spawn position
+        
+        // Generate random ball index and random spawn position.
         var spawnPos = new Vector3(Random.Range(SpawnLimitXLeft, SpawnLimitXRight), SpawnPosY, 0);
         
-        // Generate a random index to spawn a random ball
+        // Generate a random index to spawn a random ball.
         var index = Random.Range(0, ballPrefabs.Length);
 
-        // instantiate ball at random spawn location
+        // instantiate ball at random spawn location.
         Instantiate(
             ballPrefabs[index], 
             spawnPos, 
             ballPrefabs[0].transform.rotation
             );
+        
+        // Invoking the method to randomize the spawn times.
+        RandomizeInterval();
+        
+        // Reusing the invoke method with the randomized spawn time.
+        Invoke(nameof(SpawnRandomBall), _randomSpawnTime);
+        
+    }
+
+    // Randomizes the interval between spawning the balls.
+    private void RandomizeInterval()
+    {
+        _randomSpawnTime = Random.Range(1.0f, 5.0f);
     }
 
 }
