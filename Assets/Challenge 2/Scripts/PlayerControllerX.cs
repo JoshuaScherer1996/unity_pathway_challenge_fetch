30364ceb32886f9541fc4fe37ead9fa64e37abd9
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-08-12 16:02:28 +0200

    finished up the bonus challenges

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    // Declaring and Initializing variables.
     public GameObject dogPrefab;
+    private const float CoolDown = 0.5f;
+    private float _nextSpawnTime;
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog. Also checks if the current time has advanced beyond the _nextSpawnTime.
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextSpawnTime)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            
+            _nextSpawnTime = Time.time + CoolDown;
         }
     }
 }
